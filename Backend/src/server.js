ea94efae6398dab7c8590071cc1e31fb73e4f0f3
Pabitra Jana <pabitrajana117@gmail.com>
2025-07-31 02:05:28 +0530

    first commit

---
+++ Backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+
+import authRoutes from "./routes/auth_route.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT,() =>{
+    console.log(`server is running on port ${PORT}`);
+    connectDB();
+});
