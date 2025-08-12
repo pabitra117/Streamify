9f4434f3b159f75a15add4417a2ed07fc1622fa2
Pabitra Jana <pabitrajana117@gmail.com>
2025-08-12 12:32:53 +0530

    feat: add routing and pages for Home, SignUp, Login, Notifications, Call, Chat, and Onboarding; integrate React Query and Axios for data fetching

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,50 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+
+import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+
+
+
 const App = () => {
+  // tanstack query
+  const {data, isLoading, error,} = useQuery({
+    queryKey:["todos"],
+
+    queryFn: async () =>{
+      const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+      return res.data;
+    },
+  });
+
+  console.log(data);
+ 
   return (
     <div className=" h-screen text-5xl" data-theme="night">
 
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
 
         
-
-
       </Routes>
+
+      <Toaster />
     </div>
-
-
   );
 };
-
 export default App;
