b964bcce68a3a3d7411b03282699cc9ef590e8ce
Pabitra Jana <pabitrajana117@gmail.com>
2025-08-15 21:48:56 +0530

    feat: add lucide-react dependency, update routing for authentication, and implement SignUpPage with form handling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
@@ -17,30 +17,36 @@ import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
   // tanstack query
-  const {data, isLoading, error,} = useQuery({
-    queryKey:["todos"],
-
+  const {data:authData, isLoading, error,} = useQuery({
+    queryKey:["authUser"],
     queryFn: async () =>{
-      const res = await axiosInstance.get("http://localhost:5001/api/auth/me");
+      const res = await axiosInstance.get("/auth/me");
       return res.data;
     },
+    retry: false, //auth check
   });
 
-  console.log(data);
+  const authUser = authData?.user
 
 
  
   return (
     <div className=" h-screen text-5xl" data-theme="night">
 
+       {/* Show your image */}
+      <img src="/i.png" alt="Video Call" />
+
+
+      {/* Your routes */}
+
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage />: <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage />: <Navigate to="/"/>} />
+        <Route path="/notifications" element={!authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path="/call" element={!authUser ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={!authUser ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/onboarding" element={!authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
 
         
       </Routes>
--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    e.preventDefault()
+  }
+
+  
+  return <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
+    <div className="border border=primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+
+      {/*SIGNUP FORM - LEFT SIDE*/}
+      <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+      {/*LOGO*/}
+      <div className="mb-4 flex items-center justify-start gap-2">
+        <ShipWheelIcon className="size-9" />
+
+      </div>
+
+      </div>
+
+
+
+    </div>
+
+  </div>;
 };
 
 export default SignUpPage;
