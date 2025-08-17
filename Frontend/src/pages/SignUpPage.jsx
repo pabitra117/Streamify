7898a290b95b38ae55ec9772589e5e5b49c05cac
Pabitra Jana <pabitrajana117@gmail.com>
2025-08-17 11:12:04 +0530

    feat: enhance SignUpPage with form handling, validation, and styling; integrate ShipWheelIcon and routing to login

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+
+
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
     fullName: "",
@@ -6,9 +10,9 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const handleInputChange = (e) => {
-    e.preventDefault()
-  }
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
 
   
   return <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
@@ -18,16 +22,92 @@ const SignUpPage = () => {
       <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
       {/*LOGO*/}
       <div className="mb-4 flex items-center justify-start gap-2">
-        <ShipWheelIcon className="size-9" />
-
+        <ShipWheelIcon className="size-9 text-primary" />
+        <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+          Streamify
+        </span>
       </div>
+      <div className="w-full">
+        <form onSubmit={handleSignup}>
+          <div className="space-y-4">
+            <div>
+              <h2 className="text-xl font-semibold">Create an account</h2>
+              <p className="text-sm opacity-70">
+                Join Streamify and start your language learning adventure!
+              </p>
+            </div>
 
+            <div className="space-y-3">
+              {/*FULLNAME*/}
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Full Name</span>
+                </label>
+                <input type="text"
+                  placeholder="Poi"
+                  className="input input-bordered w-full"
+                  value={signupData.fullName}
+                  onChange={(e) => setSignupData({ ...signupData, fullName: e.target.value })}
+                  required
+                />
+              </div>
+              {/*EMAIL */}
+               <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email"
+                  placeholder="poi@gmail.com"
+                  className="input input-bordered w-full"
+                  value={signupData.email}
+                  onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
+                  required
+                />
+              </div>
+              {/*PASSWORD*/}
+                  <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password"
+                  placeholder="********"
+                  className="input input-bordered w-full"
+                  value={signupData.password}
+                  onChange={(e) => setSignupData({ ...signupData, password: e.target.value })}
+                  required
+                />
+                <p className="text-xs text-70 mt-1">
+                  Password must be at least 6 characters long
+                </p>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer justify-start gap-2">
+                  <input type="checkbox" className="checkbox checkbox-sm" required />
+                  <span className="text-xs leading-tight">
+                    I agree to the{""}
+                    <span className="text-primary hover:underline">terms of service</span> and{""}
+                    <span className="text-primary hover:underline">privacy policy</span>
+                  </span>
+                </label>
+              </div>
+            </div>
+
+            <button className="btn btn-primary w-full" type="submit">
+              Create Account
+            </button>
+            <div className="text-center mt-4">
+              <p className="text-sm">
+                Already have an account?{""}
+                <Link to="/login" className="text-primary hover:underline">
+                Sign in
+                </Link>
+              </p>
+            </div>
+          </div>
+        </form>
+       </div>
       </div>
-
-
-
     </div>
-
   </div>;
 };
 
